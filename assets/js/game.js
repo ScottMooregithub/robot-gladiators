dc7c231b416654ab10fdffdf62790ef095610f01
Scott Moore <sm91srjc@gmail.com>
2022-03-30 17:26:33 -0700

    update

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,13 +74,20 @@ var fight = function (enemyName) {
     //sdulting message tot the console so we know that it worked.
   }
 };
-for (var i = 0; i < enemyNames.length; i++) {
-  if (playerHealth > 0) {
-    window.alert("Welcome to Robot Gladiators! Round" + (i + 1));
-  } else {
-    window.alert("You have lost your robot in battle! Game Over!");
+var startGame = function () {
+  //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for (var i = 0; i < enemyNames.length; i++) {
+    startGame();
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round" + (i + 1));
+      var pickedEnemyName = enemyNames[i];
+      enemyHealth = 50;
+      fight(pickedEnemyName);
+    } else {
+      window.alert("You have lost your robot in battle! Game Over!");
+    }
   }
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
-}
+};
